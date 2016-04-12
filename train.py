b47f1e4ccdbb0ba1bc2c14a4b7c7535d9502696c
Donald Parker <DonaldParkerII@gmail.com>
2016-04-11 22:18:57 -0700

    find uglies

--- train.py
+++ train.py
@@ -12,10 +12,11 @@ def store_raw_images():
   neg_images_urls = urllib.request.urlopen(neg_images_links).read().decode()
   
   pos_images_links = 'http://image-net.org/api/text/imagenet.synset.geturls?wnid=n02823428'
+  
   if not os.path.exists('neg'):
     os.makedirs('neg')
-  print('ready')
-  pic_num = 1
+
+  pic_num = 532
   for i in neg_images_urls.split('\n'):
     try:
       print(i)
@@ -29,3 +30,19 @@ def store_raw_images():
       print(str(e ))
       
 store_raw_images()
+def find_uglies():
+  for file_type in ['neg']:
+    for img in os.listdir(file_type):
+      for ugly in os.listdir('uglies'):
+        try:
+          curr_img_path = str(file_type) + '/' + str(img)
+          ugly_img = cv2.imread('uglies/' + str(ugly))
+          question_img = cv2.imread(curr_img_path)
+          
+          if ugly_img.shape == question_img.shape and not(np.bitwise_xor(ugly_img, question_img).any()):
+            print('removing ' + curr_img_path)
+            os.remove(curr_img_path)
+            
+        except Exception as e:
+          print(str(e))
+# find_uglies()
